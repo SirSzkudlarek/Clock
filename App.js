ff578209496f434c810132c3006f9b0c2a9192cd
Damian Szkudlarek <damian.szkudlarek@yuush.com>
2024-04-21 23:18:20 +0200

    Beginning creation of the Clock

---
+++ App.js
@@ -0,0 +1,9 @@
+class Clock extends React.Component {
+  state = {};
+
+  render() {
+    return <div class="main-wrapper"></div>;
+  }
+}
+
+ReactDOM.render(<Clock />, document.getElementById("root"));
